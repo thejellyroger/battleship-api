ef4ac91e9ff663e373d571b73808745806e4c7af
Uni <uni@pop-os.localdomain>
2020-06-20 23:35:03 +0200

    hide uri

---
+++ db_connection.js
@@ -0,0 +1,4 @@
+const uri = 'mongodb+srv://frittimisti:lor1ale2giu3@battleship-xlj4m.mongodb.net/test?retryWrites=true&w=majority';
+module.exports = uri;
+
+// const uri = 'mongodb+srv://frittimisti:lor1ale2giu3@battleship-xlj4m.mongodb.net/test?retryWrites=true&w=majority';
